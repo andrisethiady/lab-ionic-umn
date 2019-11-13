3cb84a8571340084b141c04832ef323eb07a856a
andrisethiady <andri.setiady11@gmail.com>
2019-11-13 10:35:24 +0700

    changelog

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlaceService } from './place.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+address = '';
+  constructor( private placsvc: PlaceService) {}
 
-  constructor() {}
 
+  ngOnInit(){
+    this.placsvc.getAddress().subscribe(
+      currAddress => {
+        this.address = currAddress;
+      }
+    )
+  }
 }
